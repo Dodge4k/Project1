0d1a2ef870054cdebdadcafca4ac4cc655448802
Innocent_Malwane <innocented00@gmail.com>
2023-09-21 13:49:10 +0200

    attempt

---
+++ tasting.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Dodge{
+    public static void main(String[] args){
+        Scanner myObj = new Scanner(System.in);
+        for i
+        System.out.println("Enter a name");
+        String test1 = myObj.nextLine();
+        System.out.println(test1+ " you are King");
+    }
+
+
+}
